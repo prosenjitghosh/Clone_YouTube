0163a1b08eb8eb6cd0899248ed4b2448e9e427e5
prosenjitghosh <prosenjitg65@gmail.com>
2023-03-01 22:33:15 +0530

    Making youtube  videopage responsive

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,16 +2,21 @@ import React from 'react';
 import { SideBar } from './SideBar';
 import { Outlet } from 'react-router-dom';
 import { Head } from './Head';
+ import { useSelector } from 'react-redux';
 
 export const Body= () => {
+   const isSideBarOpen = useSelector((store)=>store.app.isSideBarOpen)
   return (
     <div>
         <Head/>
         <div className='flex'>
-            <div className="w-[18rem]">
+            <div className={ isSideBarOpen ? "basis-1/6" : "basis-0"}>
                 <SideBar/>
             </div>
-            <Outlet/>
+            <div className="basis-full">
+              <Outlet/>
+            </div>
+            
         </div>
     </div>
   )
--- src/components/WatchVideoPage.js
+++ src/components/WatchVideoPage.js
@@ -17,17 +17,24 @@ export default function WatchVideoPage() {
   },[])
 
   return (
-    <div className="mx-16 my-6">
-      <div className="flex">
-        <iframe 
-          width="900" 
-          height="500" 
-          src={video}
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <div className="mx-4 my-6 md:mx-16 ">
+      <div className="flex flex-col sm:flex-row">
+        <div className="basis-4/6">
+          <div className="aspect-video">
+            <iframe 
+              className="h-full w-full"
+              src={video}
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write;
+              encrypted-media; gyroscope; picture-in-picture; web-share" 
+              allowfullscreen></iframe>
+            </div>
+        </div>
 
-        <LiveChat/>
+        <div className="basis-2/6">
+          <LiveChat/>
+        </div>
       </div>
 
       <div className="py-5">
